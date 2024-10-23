56ed50f4b1f0f8c0c9a10c6ce837d05ede3049fb
simigami <46891822+simigami@users.noreply.github.com>
2024-10-23 16:36:38 +0900

    modify: working on mecab tokenizer

--- src/utils.py
+++ src/utils.py
@@ -23,7 +23,7 @@ def set_seed(seed: int = 42):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    #torch.use_deterministic_algorithms(True)
+    torch.use_deterministic_algorithms(True)
 
 
 def postprocess_qa_predictions(
